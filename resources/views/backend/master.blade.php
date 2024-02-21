527abd02527a869d40d779e529f548b8d47ad887
unknown <m.anowarul1994@gmail.com>
2024-02-21 22:56:13 +0600

    feat:Dashboard mastering done

---
+++ resources/views/backend/master.blade.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  @include('backend.includes.style')
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Jan 29 2024 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+ @include('backend.includes.header')
+
+  <!-- ======= Sidebar ======= -->
+ @include('backend.includes.sidebar')
+
+  <main id="main" class="main">
+
+    @yield('content')
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  @include('backend.includes.footer')
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  @include('backend.includes.script')
+
+</body>
+
+</html>
